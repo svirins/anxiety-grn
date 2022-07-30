c8877de7481ecd697c9d50680bfca7f71fa1cab7
zvirinz <zvirinz@outlook.com>
2022-07-30 22:37:09 +0300

    refactor: minor styling

--- src/components/post-title.tsx
+++ src/components/post-title.tsx
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="text-6xl md:text-4xl lg:text-6xl font-semibold capsize  md:leading-none mb-12 text-left">
+    <h1 className="text-4xl md:text-6xl font-semibold capsize  md:leading-none mb-6 text-left">
       {children}
     </h1>
   );
